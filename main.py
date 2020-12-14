c0db36e44ab7aef211614782c26ff2e874195310
TOmI mARKO <tomekmarkowicz3@wp.pl>
2020-12-14 23:29:00 +0100

    axes

--- main.py
+++ main.py
@@ -14,25 +14,16 @@ def calculateDiff(v1, v2):
     diff /= length
     return diff
 
-vectors = csv_parser.read_input('SN_projekt3/large-25x25.csv')
-width = 25
-height = 25
+vectors = csv_parser.read_input('SN_projekt3/OCRA-12x30-cut.csv')
+width = 12
+height = 30
 
-countOfPatterns = 6
+countOfPatterns = 7
 
 hn = HopfieldNetwork(height*width, countOfPatterns, False, 0.1, 1)
 
 result = hn.train(vectors)
 print("Result: ", result)
-# Z = hn.Z[0]
-# print(Z)
-# z0 = Img.vector_to_img(Z, height, width, False)
-# z1 = Img.vector_to_img(hn.Z[1], height, width, True)
-# z2 = Img.vector_to_img(hn.Z[2], height, width, True)
-# z3 = Img.vector_to_img(hn.Z[3], height, width, True)
-# z4 = Img.vector_to_img(hn.Z[4], height, width, True)
-# z5 = Img.vector_to_img(hn.Z[5], height, width, True)
-
 f, axarr = plt.pyplot.subplots(countOfPatterns, 2)
 
 for i in range(countOfPatterns):
@@ -40,6 +31,14 @@ for i in range(countOfPatterns):
     axarr[i, 1].imshow(Img.vector_to_img(hn.Z[i], height, width, False))
     diff = calculateDiff(vectors[i], hn.Z[i])
     plt.pyplot.sca(axarr[i, 1])
-    plt.pyplot.yticks([1], [diff])
+    plt.pyplot.yticks([1], ["%.2f"%diff])
+    plt.pyplot.xticks([1], [""])
+    plt.pyplot.sca(axarr[i, 0])
+    plt.pyplot.yticks([1], [""])
+    plt.pyplot.xticks([1], [""])
 
+plt.pyplot.sca(axarr[countOfPatterns - 1, 0])
+plt.pyplot.xticks([1], ["input"])
+plt.pyplot.sca(axarr[countOfPatterns - 1, 1])
+plt.pyplot.xticks([1], ["output"])
 plt.pyplot.show()
